fa8fe82b0d95661048ce85cf449c81456c9c14eb
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-05-15 12:35:29 +0200

    Portfolio

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const data = {
+  portfolios: [
+    {
+      techStack: "React, TypeScript, TMDB API, CSS",
+      title: "Movie Search App",
+      githubLink: "https://github.com/MykytaBurenok/Movie-Search-App",
+      projectLink: "https://movie-search-app-tmdb-project.vercel.app/",
+      imgLink: "images/portfolio/Tmdb.png",
+    },
+    {
+      techStack: "HTML, CSS, JavaScript, Open-Meteo API",
+      title: "Weather Dashboard",
+      githubLink: "https://github.com/MykytaBurenok/Weather-Dashboard",
+      projectLink: "https://mykytaburenok.github.io/Weather-Dashboard/",
+      imgLink: "images/portfolio/Weather.png",
+    },
+    {
+      techStack: "HTML, CSS, JavaScript",
+      title: "Web Studio Landing Page",
+      githubLink:
+        "https://github.com/MykytaBurenok/WebStudio-Landing-Page-Project",
+      projectLink:
+        "https://mykytaburenok.github.io/WebStudio-Landing-Page-Project/",
+      imgLink: "images/portfolio/Webstudio.png",
+    },
+    {
+      techStack: "HTML, CSS, JavaScript",
+      title: "Mebleria Landing Page",
+      githubLink:
+        "https://github.com/MykytaBurenok/Mebleria-landing-online-store",
+      projectLink:
+        "https://volodymyr-but2025.github.io/Progect_team-PlusUltra/",
+      imgLink: "images/portfolio/mebleria.png",
+    },
+  ],
+};
